7fb4509bfcdad8e1bc912f95a15d64edef6fecd9
myqinshimoon <42951628+myqinshimoon@users.noreply.github.com>
2018-11-30 17:43:19 +0800

    Add files via upload

---
+++ spot.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    var canvas = document.getElementById('canvas');
+
+    var engine = new Shape.Engine(canvas);
+    var promise = new Promise((resolve) => { resolve(); });
+    
+    document.getElementById('go').addEventListener('click', start);
+    
+    function start() {
+        document.getElementById('go').removeEventListener('click', start);
+        promise.then(() => engine.toText('管腾飞'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('☼'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('l'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('o'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('v'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('e'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('U'))
+        .then(() => engine.shake())
+        .then(() => engine.clear())
+        .then(() => document.getElementById('go').addEventListener('click', start));
+    }
+})();
